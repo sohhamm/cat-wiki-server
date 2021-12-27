e3b9714990c04b3b23a5fade55af29edf747c48d
Soham Sarkar <soham.ss1@gmail.com>
2021-12-27 23:25:07 +0530

    basic setup

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+// import { photoRoutes } from "./routes/index";
+// import { connectDB } from "./db";
+dotenv.config();
+
+const app: Express = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// health check route
+app.get("/", (_, res) => {
+  res.send("Health Check");
+});
+
+// app.use("/api/photos", photoRoutes);
+
+// main function
+(async () => {
+  try {
+    // await connectDB();
+    // console.log("🚀 connected to the database...");
+    app.listen(PORT, () => {
+      console.log(`🔥 server listening at http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("😞 DB connection failed");
+    console.error(err);
+  }
+})();
